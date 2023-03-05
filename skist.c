e9baccdc7a8c510d9d3c588c0cc1095e8fdd95d3
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-03-05 22:51:33 +0800

    Add files via upload

---
+++ skist.c
@@ -0,0 +1,22 @@
+#include"slist.h"
+void SLTPrint(SLTNode* phead)
+{
+	SLTNode* cur = phead;
+	while (cur != NULL)
+	{
+		printf("%d->", cur->data);
+		cur = cur->next;
+	}
+	printf("NULL\n");
+}
+void SLPushBack(SLTNode* phead, SLTDataType x)
+{
+	SLTNode* newnode = (SLTNode*)malloc(sizeof(SLTNode));
+	if (newnode == NULL)
+	{
+		perror("malloc fail");
+		return;
+	}
+	newnode->data = x;
+	newnode->next = NULL;
+}
